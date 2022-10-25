46882b195b0a3696ba37c6da999b4e4deffcad03
Henzolina <henokrobale@gmail.com>
2022-10-25 12:53:01 -0500

    list.js-added

---
+++ pages/api/list.js
@@ -0,0 +1,26 @@
+import { connectToDB } from "../../lib/connectToDB";
+
+export default async function handler(request, response){
+    try {
+        const { mongoClient } = await connectToDB;
+        const db = mongoClient.db("sample_restaurants");
+        const collection = db.collection("restaurants");
+        const results = await collection
+            .find({})
+            .project({
+                grades: 0,
+                borough: 0,
+                restaurant_id: 0,
+            })
+            .limit(10)
+            .toArray();
+
+        response.status(200).json(results);
+
+
+        
+    } catch (e) {
+        console.error(e);
+        
+    }
+}
